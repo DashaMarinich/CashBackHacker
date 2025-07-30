3c2025d2e8c118b88c985fc2386dc132df851708
Mar <dar.marinich@gmail.com>
2025-07-30 18:03:54 +0500

    test added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -17,4 +17,33 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
 
     }
+
+    @org.testng.annotations.Test
+    public void addAmountTest() {
+
+        CashbackHackService CashbackHackService = new CashbackHackService();
+
+        int amount = 100;
+
+        int actual = CashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual,expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void notAddAmountTest() {
+
+        CashbackHackService CashbackHackService = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = CashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+
+    }
+
 }
